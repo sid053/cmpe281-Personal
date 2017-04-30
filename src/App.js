0768fa8c9cfa3f871d95bfe41d7b2ef674ff604a
siddharth suthar <sid@siddharths-MacBook-Pro.local>
2017-04-30 02:06:44 -0700

    Grid implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { 
+  Row, 
+  Col, 
+  Grid,
+  Thumbnail
+ 
+} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+     <div>
+  
+
+  <Grid>
+  <Row>
+  <Col xs={6} md={3}>
+    <Thumbnail href="#" alt="171x180" src="https://s3-us-west-1.amazonaws.com/cmpe281starbuckscarouselimages/image3.JPG" />
+  </Col>
+  <Col xs={6} md={3}>
+    <Thumbnail href="www.google.com" alt="171x180" src="https://s3-us-west-1.amazonaws.com/cmpe281starbuckscarouselimages/image3.JPG" />
+  </Col>
+  <Col xs={6} md={3}>
+    <Thumbnail href="#" alt="171x180" src="https://s3-us-west-1.amazonaws.com/cmpe281starbuckscarouselimages/image3.JPG" />
+  </Col>
+  </Row>
+</Grid>
+
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import header from './Header'
+//import 'bootstrap/dist/css/bootstrap.css';
+//import 'node_modules/react-bootstrap/dist/react-bootstrap.css'
 ReactDOM.render(
+  // <header />,	
   <App />,
+  
+
   document.getElementById('root')
 );
